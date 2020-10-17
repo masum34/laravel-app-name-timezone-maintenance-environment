a26aff709809c606de5b62cfdfc8a103a8f171c2
Masum Biswas <masum0148@gmail.com>
2020-10-17 11:52:23 +0600

    App name changed

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'This rep - developed by Masum Biswas'),
 
     /*
     |--------------------------------------------------------------------------
